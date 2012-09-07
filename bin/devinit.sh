7c68c9553503bc219b7341a3c71063fb4def2d66
dcaruana <dcaruana@alfresco>
2012-09-07 22:04:33 +0100

    Add utils script - support for inProject

--- bin/devinit.sh
+++ bin/devinit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# MongoDB
-export MONGO_HOME=$DEV_HOME/software/mongodb-osx-x86_64-1.8.1
-
-# Path
-export PATH=$PATH:$MONGO_HOME/bin
+# Dev Env
+export DEV_BIN_HOME=$DEV_HOME/projects/devenv/bin
 
 # Aliases
 alias create="source create.sh"
@@ -15,6 +12,14 @@ alias couchdb="source couchdb.sh"
 alias eclipse="source eclipse.sh"
 alias dev="source dev.sh"
 
+# MongoDB
+export MONGO_HOME=$DEV_HOME/software/mongodb-osx-x86_64-1.8.1
+
+# Path
+export PATH=$PATH:$MONGO_HOME/bin
+
+# Enter the house
 cd $DEV_HOME
 
+# Change terminal title
 echo -n -e "\033]0;Dev Env\007"
