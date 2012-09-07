43d1282bafea03e36551376262055aa850b1e74b
dcaruana <dcaruana@alfresco>
2012-09-07 18:27:34 +0100

    Fix location of Eclipse data folder

--- bin/eclipse.sh
+++ bin/eclipse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$DEV_HOME/software/eclipse/eclipse -data $DEV_PROJECT/workspace -showlocation 1>$DEV_HOME/software/eclipse/eclipse.out.log 2>$DEV_HOME/software/eclipse/eclipse.err.log &
+$DEV_HOME/software/eclipse/eclipse -data $DEV_PROJECT_HOME/workspace -showlocation 1>$DEV_HOME/software/eclipse/eclipse.out.log 2>$DEV_HOME/software/eclipse/eclipse.err.log &
