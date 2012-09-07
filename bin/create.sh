d586938b64dbd3605efb7434ef4d98b5d6f5d76e
dcaruana <dcaruana@alfresco>
2012-09-07 18:53:24 +0100

    Create template project folder structure - ignore workspace folder

--- bin/create.sh
+++ bin/create.sh
@@ -7,6 +7,6 @@ elif [[ -d $DEV_HOME/projects/$1 ]] ; then
     echo Project $1 already exists
 else
     mkdir $DEV_HOME/projects/$1
-    mkdir $DEV_HOME/projects/$1/workspace
+    cp -r $DEV_HOME/projects/devenv/devenv_template/ $DEV_HOME/projects/$1
     workon $1
 fi
